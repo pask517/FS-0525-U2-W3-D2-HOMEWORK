e2e945e37bcf23153f7bd4a5ac4bc579f262d45d
pask517 <andreapascarella99@gmail.com>
2025-11-04 16:26:55 +0100

    updated

--- scripts/homework.js
+++ scripts/homework.js
@@ -1,14 +1,17 @@
 const textField = document.getElementById("textField")
 const namesList = document.getElementById("namesList")
-//const createLi = function () {
-// namesList.innerHTML = `<li class="text-capitalize">${textField.value}</li>`
-//}
 
 const savingForm = document.getElementById("savingForm")
 const saveButton = document.getElementById("saveButton")
+const savedName = localStorage.getItem("Saved Name")
+namesList.innerHTML = `<li class="text-capitalize">${savedName}</li>`
+if (savedName === null) {
+  namesList.innerHTML = `<li></li>`
+}
 saveButton.addEventListener("click", (e) => {
   e.preventDefault()
   localStorage.setItem("Saved Name", textField.value)
+
   namesList.innerHTML = `<li class="text-capitalize">${textField.value}</li>`
   savingForm.reset()
 })
